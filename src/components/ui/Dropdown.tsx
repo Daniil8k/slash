574871a72d0e6aaff8da7bf3003aa2bd74f50256
Daniil <93822098+Daniil8k@users.noreply.github.com>
2022-07-15 15:41:39 +0300

    dropdown

---
+++ src/components/ui/Dropdown.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+
+export interface DropdownProps {
+	data: any[];
+	onSelect: (item: any) => void;
+	className: string;
+	children: React.ReactNode;
+}
+
+const Dropdown: FC<DropdownProps> = ({
+	className,
+	children,
+	data,
+	onSelect,
+}) => {
+	return (
+		<div className={["dropdown", className].join(" ")}>
+			{children}
+			<div className="dropdown-content">
+				{data.map((item) => (
+					<button onClick={() => onSelect(item)}>{item}</button>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default Dropdown;
