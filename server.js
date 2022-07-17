fe7184c1941e690c87f072ff048dd678c6e1f4ba
Daniil <93822098+Daniil8k@users.noreply.github.com>
2022-07-17 15:53:13 +0300

    server update

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const express = require("express");
+import express from "express";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const port = process.env.PORT || 8080;
 const app = express();
 
