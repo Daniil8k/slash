b0163c79d079eb6151a377c124449bb661b3261d
Daniil <93822098+Daniil8k@users.noreply.github.com>
2022-07-19 15:51:31 +0300

    binance websocket link

--- src/components/app/Panel.tsx
+++ src/components/app/Panel.tsx
@@ -8,12 +8,15 @@ interface PanelProps {
 const Panel: FC<PanelProps> = ({ className }) => {
 	return (
 		<div
-			className={[
-				"card flex flex-col items-center px-2",
-				className,
-			].join(" ")}
+			className={["card flex flex-col items-start px-2", className].join(" ")}
 		>
-			<div className="mb-4">Coin Market</div>
+			<a
+				href="https://binance-docs.github.io/apidocs/#websocket-market-streams"
+				target="_blank"
+				className="mb-4 hover:underline"
+			>
+				WebSocket Binance Stream
+			</a>
 			<Ticker />
 		</div>
 	);
